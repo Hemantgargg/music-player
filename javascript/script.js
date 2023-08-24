913000d56cb2034cebf08fa88603486e5b82a7a7
hemant garg <gargyhemant1@gmail.com>
2023-08-24 23:43:03 +0530

    s

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,45 @@
 console.log("welcome to my music");
 
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('./javascript/1.mp3');
 
-audioElement.play();
+// document.getElementById("masterPlay").addEventListener('click', () => {
 
-
-
-// let masterPlay = document.getElementById('masterPlay');
-// let myProgressBar = document.getElementById('myProgressBar');
-
-// let songs = [
-//     { songName: "dope shop", filePath: "song/1.mp3" }
-
-// ]
-
-
-// masterPlay.addEventListener('click', () => {
-//     if (audioElement.paused || audioElement.currentTime <= 0) {
-//         audioElement.play();
-//     }
-
+//     audioElement.play();
+    
 // })
 
-// myProgressBar.addEventListener('timeupdate', () => {
-//     console.log('timeupdate');
 
-// })
+
+// audioElement.play();
+
+
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
+let songs = [
+    { songName: "dope shop", filePath: "song/1.mp3" }
+
+]
+
+
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-regular', 'fa-2x' , 'fa-circle-play');
+        masterPlay.classList.add('fa-solid' ,'fa-2x' , 'fa-pause');
+
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-solid' ,'fa-2x' , 'fa-pause');
+        masterPlay.classList.add('fa-regular', 'fa-2x' , 'fa-circle-play');
+       
+    }
+
+})
+
+myProgressBar.addEventListener('timeupdate', () => {
+    console.log('timeupdate');
+
+})
