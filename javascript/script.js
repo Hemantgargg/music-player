94b98b959630de9abe0e1991c00a728f4866f3ba
hemant garg <gargyhemant1@gmail.com>
2023-08-24 19:46:24 +0530

    project started

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+console.log("welcome to my music");
+
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+
+audioElement.play();
+
+// let masterPlay = document.getElementById('masterPlay');
+// let myProgressBar = document.getElementById('myProgressBar');
+
+// let songs = [
+//     { songName: "dope shop", filePath: "song/1.mp3" }
+
+// ]
+
+
+// masterPlay.addEventListener('click', () => {
+//     if (audioElement.paused || audioElement.currentTime <= 0) {
+//         audioElement.play();
+//     }
+
+// })
+
+// myProgressBar.addEventListener('timeupdate', () => {
+//     console.log('timeupdate');
+
+// })
